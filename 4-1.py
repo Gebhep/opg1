5b0785d565ceb9c907170b5cbafc73f1a14043a3
Gebhep <161262262+Gebhep@users.noreply.github.com>
2024-09-11 08:03:19 +0200

    Add files via upload

---
+++ 4-1.py
@@ -0,0 +1,10 @@
+from machine import Pin #Importerer Pin klassen fra machine modulet
+from time import sleep #Importerer sleep klassen fra time modulet
+
+red_led = Pin(26, Pin.OUT) # Laver variabel for Pin 26 som et output
+
+while True:
+    red_led.value(1) # Tænder LED
+    sleep(1) # Venter 1 sekund
+    red_led.value(0) # Slukker LED
+    sleep(1) # Venter 1 sekund
